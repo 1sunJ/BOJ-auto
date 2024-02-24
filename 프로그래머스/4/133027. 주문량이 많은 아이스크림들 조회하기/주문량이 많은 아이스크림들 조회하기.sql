87c27a40b7ec530d59e7d31e3f5fe1c1a8fccab9
1sunJ <141487618+1sunJ@users.noreply.github.com>
2024-02-24 12:25:37 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,6 +1,6 @@
-select J.flavor
+select H.flavor
 from first_half H, july J
 where H.flavor = J.flavor
-group by J.flavor
+group by H.flavor
 order by sum(H.total_order) + sum(J.total_order) desc
 limit 3
