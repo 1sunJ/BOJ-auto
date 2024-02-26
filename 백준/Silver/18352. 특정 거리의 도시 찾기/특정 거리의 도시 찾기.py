d42255ca589da7aafda77ba790d2d48282bfd006
1sunJ <141487618+1sunJ@users.noreply.github.com>
2024-02-26 17:40:23 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 2004 ms, Memory: 98668 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,34 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N, M, K, X = map(int, input().split())
+adjList = [[] for _ in range(N+1)]
+for i in range(M) :
+    a, b = map(int, input().split())
+    adjList[a].append(b)
+
+visited = [False] * (N+1)
+visited[X] = 1
+que = deque([X])
+
+while que :
+    x = que.popleft()
+
+    for y in adjList[x] :
+        if visited[y] :
+            continue
+
+        visited[y] = visited[x] + 1
+        que.append(y)
+
+# print("visited", visited)
+
+existFlag = False
+for i in range(1, N+1) :
+    if visited[i] == K+1 :
+        print(i)
+        existFlag = True
+
+if not existFlag :
+    print(-1)
