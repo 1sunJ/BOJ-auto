44e899bc7a8a870b1ade3672232d70c15999b803
1sunJ <141487618+1sunJ@users.noreply.github.com>
2024-03-02 23:55:22 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.py
@@ -0,0 +1,8 @@
+s=input()
+for i in range(3) :
+    for j in range(3) :
+        if i == 1 and j == 1 :
+            print(':'+s+':', end='')
+            continue
+        print(':fan:',end='')
+    print()
