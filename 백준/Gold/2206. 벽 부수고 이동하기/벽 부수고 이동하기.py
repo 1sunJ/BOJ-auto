0ed3642b1bd9760b28d2c074d6c7f5456a2c5976
1sunJ <141487618+1sunJ@users.noreply.github.com>
2024-03-27 10:58:28 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 5856 ms, Memory: 189864 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,67 +1,52 @@
 from collections import deque
-import sys
-
-input = sys.stdin.readline
-MOVE = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+MOVE = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
 N, M = map(int, input().split())
-mtx = [list(map(int, input().rstrip())) for _ in range(N)]
+K = 1
+mtx = [list(map(int, input())) for _ in range(N)]
 
-visited = [[[0]*2 for _ in range(M)] for _ in range(N)]
+visited = [[[0] * (K+1) for _ in range(M)] for _ in range(N)]
 visited[0][0][0] = 1
-visited[0][0][1] = 1
-que = deque([])
-que.append([0, 0])
 
-# t = 150
+que = deque([(0, 0, 0)])
+
 while que :
-    x, y = que.popleft()
-    # print("!!", x, y, que)
+    x, y, z = que.popleft()
+    # print('x, y, z', x, y, z)
+
+    findAnswer = False
+    for v in visited[N-1][M-1] :
+        if v :
+            findAnswer = True
+            break
+    if findAnswer :
+        # print("!! FIND !!")
+        break
 
     for xx, yy in MOVE :
         xx, yy = x + xx, y + yy
-
-        if not 0 <= xx < N or not 0 <= yy < M or (visited[xx][yy][0] and visited[xx][yy][1]) :
+        if not 0 <= xx < N or not 0 <= yy < M :
             continue
+        
+        # to 길
+        if mtx[xx][yy] == 0 :
+            if not visited[xx][yy][z] :
+                visited[xx][yy][z] = visited[x][y][z] + 1
+                que.append((xx, yy, z))
 
-        if visited[x][y][0] and not visited[xx][yy][0] and visited[xx][yy][1] :
-            if not mtx[xx][yy] :
-                visited[xx][yy][0] = visited[x][y][0] + 1
-                que.append([xx, yy])
-                continue
+        # to 벽
+        else :
+            if z < K and not visited[xx][yy][z+1]:
+                visited[xx][yy][z+1] = visited[x][y][z] + 1
+                que.append((xx, yy, z+1))
 
-        if not visited[xx][yy][0] and not visited[xx][yy][1] :
-            # 벽인 경우
-            if mtx[xx][yy] :
-                # 갈 수 있음
-                if visited[x][y][0] :
-                    visited[xx][yy][1] = visited[x][y][0] + 1
-                    que.append([xx, yy])
+    # print("==================")
+    # for v in visited :
+    #     print(v)
 
-            # 아닌 경우
-            else :
-                if visited[x][y][0] :
-                    visited[xx][yy][0] = visited[x][y][0] + 1
-                
-                if visited[x][y][1] :
-                    visited[xx][yy][1] = visited[x][y][1] + 1
-                else :
-                    visited[xx][yy][1] = visited[xx][yy][0]
+for x in visited[N-1][M-1] :
+    if x :
+        print(x)
+        exit()
 
-                if visited[xx][yy][0] or visited[xx][yy][1] :
-                    que.append([xx, yy])
-
-    # print(*visited, sep='\n')
-    # print("==============")
-    # t -= 1
-    # if not t : break
-
-if visited[N-1][M-1][0] == 0 and visited[N-1][M-1][1] == 0 :
-    print(-1)
-else :
-    if visited[N-1][M-1][0] == -1 or visited[N-1][M-1][0] == 0 :
-        print(visited[N-1][M-1][1])
-    elif visited[N-1][M-1][1] == -1 or visited[N-1][M-1][1] == 0 :
-        print(visited[N-1][M-1][0])
-    else :
-        print(min(visited[N-1][M-1]))
+print(-1)
