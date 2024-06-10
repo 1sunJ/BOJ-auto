03336b91b3a5b6129df3a406b8cd9397dd0d0335
1sunJ <141487618+1sunJ@users.noreply.github.com>
2024-06-10 17:53:11 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 240 ms, Memory: 36616 KB -BaekjoonHub

---
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.py
@@ -0,0 +1,52 @@
+import sys
+input = sys.stdin.readline
+
+def toDecimal(x) :
+    return int(x, 2)
+
+def toBinary(n) :
+    return bin(n)
+
+N, M = map(int, input().split())
+
+arr = [1] + [bin(2**20)] * N
+
+for _ in range(M) :
+    INPUT = input().rstrip()
+    if INPUT.count(' ') == 2 :
+        oper, i, x = map(int, INPUT.split())
+    else :
+        oper, i = map(int, INPUT.split())
+
+    if oper == 1 :
+        if arr[i][-x] == '0' :
+            n = toDecimal(arr[i]) + 2 ** (x-1)
+            arr[i] = toBinary(n)
+    elif oper == 2:
+        if arr[i][-x] == '1' :
+            n = toDecimal(arr[i]) - 2 ** (x-1)
+            arr[i] = toBinary(n)
+    elif oper == 3:
+        n = toDecimal(arr[i])
+        if arr[i][-20] == '1' :
+            n -= 2 ** 19
+
+        n -= 2 ** 20
+        n = (n << 1) + 2 ** 20
+        arr[i] = toBinary(n)
+    else :
+        n = toDecimal(arr[i])
+        if arr[i][-1] == '1' :
+            n -= 2 ** 0
+
+        n -= 2 ** 20
+        n = (n >> 1) + 2 ** 20
+        arr[i] = toBinary(n)
+
+    # print(*arr, sep = '\n')
+
+answer = set()
+for x in arr[1::] :
+    answer.add(int(x, 2))
+
+print(len(answer))
