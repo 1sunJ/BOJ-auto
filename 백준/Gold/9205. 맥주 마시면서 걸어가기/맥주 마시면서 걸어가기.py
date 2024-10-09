ce6e894dd34e87f5431bd13940677292895cd1dd
1sunJ <141487618+1sunJ@users.noreply.github.com>
2024-10-09 17:21:41 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 116 ms, Memory: 34072 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+for _ in range(int(input())) :
+    n = int(input())
+    sx, sy = map(int, input().split())
+    cons = [list(map(int, input().split())) for _ in range(n)]
+    ex, ey = map(int, input().split())
+
+    que = deque([(sx, sy)])
+    visited = [False] * n
+
+    result = False
+    while que :
+        sxx, syy = que.popleft()
+        
+        if abs(sxx - ex) + abs(syy - ey) <= 1000 :
+            result = True
+            break
+
+        for i in range(n) :
+            cx, cy = cons[i]
+            if abs(cx - sxx) + abs(cy - syy) > 1000 or visited[i] :
+                continue
+
+            visited[i] = True
+            que.append((cx, cy))
+
+    print("happy" if result else "sad")
