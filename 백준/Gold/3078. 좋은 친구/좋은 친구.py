5b5b4cefceb29c20e16a69913f0f0d0f578d6ef0
1sunJ <141487618+1sunJ@users.noreply.github.com>
2024-10-10 16:32:36 +0900

    [Gold IV] Title: 좋은 친구, Time: 192 ms, Memory: 38196 KB -BaekjoonHub

--- 백준/Gold/3078. 좋은 친구/좋은 친구.py
+++ 백준/Gold/3078. 좋은 친구/좋은 친구.py
@@ -3,23 +3,16 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
-arr = [-1] * K + [len(input()) for _ in range(N)]
-
-l, r = 0, K
-map = {arr[K] : 1, -1 : -1}
+arr = [len(input().rstrip()) for _ in range(N)] + [-1] * (K + 1)
+count = [0] * 22
 
 answer = 0
-for i in range(K + 1, N + K) :
-    # 탈락
+for i in range(N) :
     old = arr[i - K - 1]
-    map[old] -= 1
+    count[old] -= 1
 
-    # 추가
     new = arr[i]
-    if new in map :
-        answer += map[new]
-        map[new] += 1
-    else :
-        map[new] = 1
+    answer += count[new]
+    count[new] += 1
 
 print(answer)
