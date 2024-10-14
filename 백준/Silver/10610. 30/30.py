440b951ee55f54d434d68a6ea1c774026d197a64
1sunJ <141487618+1sunJ@users.noreply.github.com>
2024-10-14 17:40:36 +0900

    [Silver IV] Title: 30, Time: 52 ms, Memory: 32252 KB -BaekjoonHub

--- 백준/Silver/10610. 30/30.py
+++ 백준/Silver/10610. 30/30.py
@@ -1,14 +1,7 @@
-def toInt(arr) :
-    n = 0
-    for x in arr :
-        n = 10 * n + x
-    return n
+N = str(input())
 
-N = int(input())
-N = sorted(list(map(int, str(N))), reverse=True)
-
-if N[-1] != 0 or sum(N) % 3 != 0:
+if not '0' in N or sum(map(int, N)) % 3 != 0 :
     print(-1)
     exit()
 
-print(toInt(N))
+print(''.join(sorted(N, reverse=True)))
