0784cc04149fac5629f0aef7adb591e53b048189
1sunJ <141487618+1sunJ@users.noreply.github.com>
2024-12-18 03:27:27 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -0,0 +1,19 @@
+select animal_id, name
+from animal_ins
+where intake_condition NOT like "Aged"
+order by animal_id
+
+
+
+
+
+
+
+
+
+
+
+# select animal_id, name
+# from animal_ins
+# where intake_condition not like 'Aged'
+# order by animal_id
