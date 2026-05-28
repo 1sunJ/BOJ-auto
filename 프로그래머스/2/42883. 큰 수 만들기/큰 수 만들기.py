6d07940e503d100a5b061770d53c906935e1869c
1sunJ <141487618+1sunJ@users.noreply.github.com>
2026-05-28 18:30:03 +0900

    [level 2] Title: 큰 수 만들기, Time: 51.41 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,17 +1,3 @@
 def solution(number, k):
-    arr = []
-    i = 0
-    while k > 0 and i < len(number) :
-        if not arr or arr[-1] >= number[i] :
-            arr.append(number[i])
-            i += 1
-        else :
-            arr.pop()
-            k -= 1
-
-    arr += list(number[i::])
-    
-    if k > 0 :
-        arr = arr[:-k:]
-
-    return ''.join(arr)
+    answer = ''
+    return answer
