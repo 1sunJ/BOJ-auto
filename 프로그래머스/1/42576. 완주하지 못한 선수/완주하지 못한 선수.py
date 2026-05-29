6d9975f3a1abc6f269260f23a45d39af7ec0daff
1sunJ <141487618+1sunJ@users.noreply.github.com>
2026-05-29 21:48:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.78 ms, Memory: 34.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,3 +1,23 @@
 def solution(participant, completion):
-    answer = ''
-    return answer
+    hash = {}
+    for x in participant:
+        if x in hash:
+            hash[x] += 1 
+        else :
+            hash[x] = 1 
+    hash2 = {}  
+    for x in completion:
+         if x in hash2:
+             hash2[x] += 1
+         else:
+             hash2[x] = 1
+
+    for x in hash2:
+        hash[x] -= hash2[x]
+        
+        if hash[x] == 0:
+            del hash[x]
+
+    return list(hash.keys())[0]
+
+   
