63ee4a247e7f0ff8f916756cb13378b62bad7042
1sunJ <141487618+1sunJ@users.noreply.github.com>
2026-06-04 18:44:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 46.69 ms, Memory: 32.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,23 +1,14 @@
 def solution(participant, completion):
-    hash = {}
-    for x in participant:
-        if x in hash:
-            hash[x] += 1 
+    part = {}
+    for x in participant :
+        if x in part :
+            part[x] += 1
         else :
-            hash[x] = 1 
-    hash2 = {}  
-    for x in completion:
-         if x in hash2:
-             hash2[x] += 1
-         else:
-             hash2[x] = 1
+            part[x] = 1
 
-    for x in hash2:
-        hash[x] -= hash2[x]
-        
-        if hash[x] == 0:
-            del hash[x]
+    for x in completion :
+        part[x] -= 1
+        if part[x] == 0 :
+            part.pop(x)
 
-    return list(hash.keys())[0]
-
-   
+    return list(part.keys())[0]
